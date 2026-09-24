16935c48e697dd574e62a5ccf404e54476000fbb
jad-fahmi <225529131+jad-fahmi@users.noreply.github.com>
2026-09-24 18:11:54 +0800

    feat: isolate PostgreSQL backup access

--- service/api/deploy/postgres-roles.sql
+++ service/api/deploy/postgres-roles.sql
@@ -3,6 +3,7 @@
 \getenv migrator_password ADUEN_MIGRATOR_PASSWORD
 \getenv api_password ADUEN_API_PASSWORD
 \getenv retention_password ADUEN_RETENTION_PASSWORD
+\getenv backup_password ADUEN_BACKUP_PASSWORD
 
 SELECT format(
   'CREATE ROLE aduen_migrator LOGIN PASSWORD %L NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT NOREPLICATION NOBYPASSRLS',
@@ -16,7 +17,12 @@ SELECT format(
   'CREATE ROLE aduen_retention LOGIN PASSWORD %L NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT NOREPLICATION NOBYPASSRLS',
   :'retention_password'
 ) \gexec
+SELECT format(
+  'CREATE ROLE aduen_backup LOGIN PASSWORD %L NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION BYPASSRLS',
+  :'backup_password'
+) \gexec
 
-GRANT CONNECT ON DATABASE :"database_name" TO aduen_migrator, aduen_api, aduen_retention;
+GRANT CONNECT ON DATABASE :"database_name" TO aduen_migrator, aduen_api, aduen_retention, aduen_backup;
 GRANT USAGE, CREATE ON SCHEMA public TO aduen_migrator;
-GRANT USAGE ON SCHEMA public TO aduen_api, aduen_retention;
+GRANT USAGE ON SCHEMA public TO aduen_api, aduen_retention, aduen_backup;
+GRANT pg_read_all_data TO aduen_backup;
